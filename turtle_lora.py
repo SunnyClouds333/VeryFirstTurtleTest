0f2265a44051bd6fc541492e49f7d96b3cef7fc5
SunnyClouds333 <129523990+SunnyClouds333@users.noreply.github.com>
2023-09-26 17:25:39 +0300

    added turtle file

---
+++ turtle_lora.py
@@ -0,0 +1,36 @@
+import turtle as t
+
+t.bgcolor('black')
+t.pencolor("white")
+t.pensize(11)
+
+t.right(100)
+t.forward(100)
+t.left(100)
+t.forward(100)
+
+t.circle(50)
+
+
+t.right(100)
+t.forward(100)
+t.backward(100)
+t.left(100)
+t.forward(100)
+t.right(100)
+t.forward(50)
+t.right(100)
+t.forward(100)
+t.right(100)
+t.isdown()
+t.right(180)
+t.goto(200, -200)
+
+t.left(50)
+t.goto(250, 50)
+t.left(90)
+t.goto(400, -200)
+t.left(120)
+t.forward(400)
+t.hideturtle()
+t.done()
